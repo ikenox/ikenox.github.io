3b54cdda767ecda79ee8170dd5775e0e29ca4898
Naoto Ikeno <ikenox@gmail.com>
2018-08-07 02:38:44 +0900

    microlink

--- src/components/PreviewLink.js
+++ src/components/PreviewLink.js
@@ -1,17 +1,47 @@
 import React from 'react'
 
 class PreviewLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
   render() {
     return (
       <a href={this.props.url}>
-        {this.props.title}
+        <div>
+          {this.state.title}
+          <br/>
+          {this.state.description}
+          <br/>
+          {this.state.imageUrl}
+          <br/>
+          {this.state.host}
+        </div>
       </a>
     )
   }
 
   componentDidMount() {
-    console.log(this.props.url)
-    console.log(this.props.title)
+    fetch(`https://api.microlink.io/?url=${ this.props.url }`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          if ("success" === result.status) {
+            this.setState({
+              title: result.data.title,
+              description: result.data.description,
+              imageUrl: result.data.image ? result.data.image.url : null,
+              host: result.data.publisher,
+            });
+          }
+        },
+        (error) => {
+          this.setState({
+          });
+        }
+      )
   }
 }
 
