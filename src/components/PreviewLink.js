8e3ce6852c5879491e1ebfd213a7ca455bced9f3
Naoto Ikeno <ikenox@gmail.com>
2018-08-07 03:12:33 +0900

    microlink style

--- src/components/PreviewLink.js
+++ src/components/PreviewLink.js
@@ -1,29 +1,43 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Info = styled.div`
+p { margin:0px;line-height:1.2em; }
+`
 
 class PreviewLink extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
   render() {
     return (
-      <a href={this.props.url}>
-        <div>
-          {this.state.title}
-          <br/>
-          {this.state.description}
-          <br/>
-          {this.state.imageUrl}
-          <br/>
-          {this.state.host}
+      <a href={this.props.url}
+         style={{display: "box", marginTop: "10px", marginBottom: "10px", textDecoration: "none"}}>
+        <div style={{border: "1px solid #ccc", display: "flex", maxWidth: "500px"}}>
+          <img src={this.state.imageUrl} style={{width: "120px", height: "120px", objectFit: "cover"}}/>
+          <Info style={{padding: ".5rem .75rem", position:"relative"}}>
+            <p style={{fontWeight: "bold"}}>{this.state.title}</p>
+            <p style={{
+              marginTop:".2rem",
+              color: "#666",
+              height:"4.2em",
+              fontSize: ".75rem",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              wordBreak: "break-all",
+              lineHeight:"1.4em"
+            }}>{this.state.description}</p>
+            <p style={{	marginBottom:".5rem", position: "absolute", bottom: 0, color: "#666", fontSize: ".7rem"}}>{this.state.host}</p>
+          </Info>
         </div>
       </a>
     )
   }
 
-  componentDidMount() {
+  componentDidMount
+  () {
     fetch(`https://api.microlink.io/?url=${ this.props.url }`)
       .then(res => res.json())
       .then(
@@ -38,8 +52,7 @@ class PreviewLink extends React.Component {
           }
         },
         (error) => {
-          this.setState({
-          });
+          this.setState({});
         }
       )
   }
