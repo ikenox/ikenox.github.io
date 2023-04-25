965ac5c01d8634ef1bd983dc4e1f451fbbf25343
naoto.ikeno <ikenox@gmail.com>
2023-04-25 21:51:01 +0900

    update

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,1 +1,12 @@
-not-found.tsx
+import { Metadata } from "next";
+import { pageInfoBase, toMetadata } from "@/app/page-info";
+
+export default function Custom404() {
+  return <h1>404 - Page Not Found</h1>;
+}
+
+// FIXME this is not working
+export const metadata: Metadata = toMetadata({
+  ...pageInfoBase,
+  title: "404 Not Found",
+});
