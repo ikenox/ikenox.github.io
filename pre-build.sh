d05fd35778af4d730874f58214affb3833a331ba
naoto.ikeno <ikenox@gmail.com>
2023-04-25 23:16:00 +0900

    fix script

--- pre-build.sh
+++ pre-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printf "copy static files..."
-rm -rf public/*
+ls public && rm -rf public/*
 cp -r static-files/* public/
 find blog -type d | grep static | xargs -I% mkdir -p public/%
 find blog -type f | grep static | xargs -I% cp % public/%
