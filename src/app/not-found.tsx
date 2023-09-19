3757358d764651a34fe5dd0482670c460215e2a2
naoto.ikeno <ikenox@gmail.com>
2023-09-19 22:54:08 +0900

    update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
-import { Metadata } from "next";
-import { pageInfoBase, toMetadata } from "@/app/page-info";
+import { Metadata } from 'next';
+import { pageInfoBase, toMetadata } from '@/app/page-info';
 
 export default function Custom404() {
   return <h1>404 - Page Not Found</h1>;
@@ -8,5 +8,5 @@ export default function Custom404() {
 // FIXME this is not working
 export const metadata: Metadata = toMetadata({
   ...pageInfoBase,
-  title: "404 Not Found",
+  title: '404 Not Found',
 });
