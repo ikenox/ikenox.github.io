afd91cab96826b9a2077200c5cca9ee914407294
naoto.ikeno <ikenox@gmail.com>
2023-10-07 15:17:35 +0900

    prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from '@astrojs/cloudflare';
+
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ikenox.info',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), prefetch()],
   markdown: {
     shikiConfig: {
       theme: 'slack-dark',
